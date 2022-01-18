4b25cd820185a894906e9b3c2d7a1d3cad7db7eb
Helena <nenafgreco@hotmail.com>
2022-01-18 10:35:48 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -10,6 +11,16 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
+  
+  if (talkers.length === 0) {
+    res.status(HTTP_OK_STATUS).json([]);
+  }
+
+  res.status(HTTP_OK_STATUS).json(talkers);
 });
 
 app.listen(PORT, () => {
